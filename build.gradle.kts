15ca48aeaf7bc22282c62dd1d10f02438b8062eb
Martin Bonnin <martin@mbonnin.net>
2025-08-15 16:03:40 +0200

    Update librarian

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,8 @@ import com.gradleup.librarian.gradle.Librarian
 
 plugins {
     alias(libs.plugins.kgp).apply(false)
-    id("com.gradleup.librarian").version("0.0.8-SNAPSHOT-b703634bb1858f471503e23d2c23d7aeae9d6120").apply(false)
+    id("com.gradleup.librarian").version("0.1.1-SNAPSHOT-e64004307778d825d359d0a6d1243f7d16ca2653").apply(false)
+    id("org.jetbrains.kotlinx.binary-compatibility-validator").version("0.18.1")
 }
 
 Librarian.root(project)
