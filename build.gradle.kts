5003aadf408cf87d928fc1ab12c8f3afdf04f040
Martin Bonnin <martin@mbonnin.net>
2025-08-15 16:24:54 +0200

    Add .editorconfig file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import com.gradleup.librarian.gradle.Librarian
 
 plugins {
-    alias(libs.plugins.kgp).apply(false)
-    id("com.gradleup.librarian").version("0.0.8-SNAPSHOT-b703634bb1858f471503e23d2c23d7aeae9d6120").apply(false)
+  alias(libs.plugins.kgp).apply(false)
+  id("com.gradleup.librarian").version("0.0.8-SNAPSHOT-b703634bb1858f471503e23d2c23d7aeae9d6120").apply(false)
 }
 
 Librarian.root(project)
